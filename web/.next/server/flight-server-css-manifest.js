8f72b36743491d94aae41c8d8b77af59396bf659
Lipevic <felipe.9000@hotmail.com>
2023-06-02 15:18:34 -0300

    initial commit project

---
+++ web/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\www\\nlw\\Aula_2\\web\\src\\app\\layout.tsx": [
+      "C:\\www\\nlw\\Aula_2\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Roboto_Flex\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-roboto\"}],\"variableName\":\"roboto\"}",
+      "C:\\www\\nlw\\Aula_2\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Bai_Jamjuree\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"700\",\"variable\":\"--font-bai-Jamjuree\"}],\"variableName\":\"baiJamjuree\"}",
+      "C:\\www\\nlw\\Aula_2\\web\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\www\\nlw\\Aula_2\\web\\src\\app\\page": [
+      "C:\\www\\nlw\\Aula_2\\web\\src\\app\\globals.css",
+      "C:\\www\\nlw\\Aula_2\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Roboto_Flex\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-roboto\"}],\"variableName\":\"roboto\"}",
+      "C:\\www\\nlw\\Aula_2\\web\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Bai_Jamjuree\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"700\",\"variable\":\"--font-bai-Jamjuree\"}],\"variableName\":\"baiJamjuree\"}"
+    ]
+  }
+}
---
+++ web/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-Jamjuree',
+})
+export const metadata = {
+  title: 'NLW SpaceTime',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e Typescript.',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
